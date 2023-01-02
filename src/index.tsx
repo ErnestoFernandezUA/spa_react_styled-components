8a6d9b0973feac5f8888e8fb375d2ff3db3d3647
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-01-02 10:19:54 +0200

    first

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './pages/App';
 import { Provider } from 'react-redux';
 import store, { persistor } from './app/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
