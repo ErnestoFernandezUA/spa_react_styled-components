ad26dee3c98a40d9723da005c978cb4ee47cf48c
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-01-04 09:10:00 +0200

    fetch country details

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,11 +1,32 @@
 import { FunctionComponent } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { IoArrowBack } from 'react-icons/io5';
+import { Country } from "../type/Country";
+import { getCountry } from "../api/country";
 
 export const Details: FunctionComponent = () => {
   let { name } = useParams();
+  const navigate = useNavigate();
+  const [country, setCountry] = useState<Country | null>(null);
+
+  useEffect(() => {
+    const fetch = async (value: string) => {
+      const response = await getCountry(value);
+      console.log(response);
+
+      setCountry(response);
+    };
+
+    if (name) {
+      fetch(name).catch(e => console.log(e));
+    }
+  }, [name])
+
 
   return (
-    <div>
+    <div onClick={() => navigate('/')}>
+      <button><IoArrowBack/>Back</button>
       Details: {name}
     </div>
   );
