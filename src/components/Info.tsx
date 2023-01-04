f9ff6fe11780f2019479df35ada4e42ef9c760ba
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-01-04 21:59:06 +0200

    working without redux-toolkit

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -11,15 +11,12 @@ const Wrapper = styled.section`
   gap: 2rem;
   align-items: start;
 
-  & > div, img {
-    // border: 1px solid red;
-  }
-
   @media(min-width: 767px){
     grid-template-columns: minmax(100px, 400px) 1fr;
     align-items: center;
     gap: 5rem;
   }
+  
   @media(min-width: 1024px){
     grid-template-columns: minmax(400px, 600px) 1fr;
   }
